e52d03703f48a5e4aba52d9bb4ceefe66becbb62
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-21 19:39:19 +0200

    Update MSGCommand.php

--- src/EasyMSG/Commands/MSGCommand.php
+++ src/EasyMSG/Commands/MSGCommand.php
@@ -8,17 +8,20 @@ use pocketmine\player\Player;
 class MSGCommand extends Command{
 
     public function __construct(){
-        parent::__construct("msg", "", "msg <playername> <Message>", []);
+        parent::__construct("msg", "", "msg <playername> <Message>", ["tell"]);
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
         if($sender instanceof Player){
             if (isset($args[0])) {
-                if(isset($args[1])){
-                    $p = $args[0];
+                if(isset($data[1])){
+                    $p = $data[0];
                     if ($p instanceof Player) {
                         $p->sendMessage($args[1]);
                     }
+                }
+                if(!$p){
+                    $sender->sendMessage("Player not found or is not online");
                 }
             }            
         }else{
