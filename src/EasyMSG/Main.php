92f1f9f0fe370b8b508e3599deba0c598394bed0
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-22 06:58:48 +0200

    Update Main.php

---
+++ src/EasyMSG/Main.php
@@ -0,0 +1,23 @@
+<?php
+namespace EasyMSG;
+
+use EasyMSG\Commands\MSGCommand;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\SingletonTrait;
+
+class Main extends PluginBase{
+    use SingletonTrait;
+
+    public function onLoad(): void{
+        self::setInstance($this);
+        self::getServer()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("tell"));
+        self::getServer()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("msg"));
+    }
+
+    public function onEnable(): void{
+        self::getServer()->getCommandMap()->registerAll($this->getName(), $this[
+            new MSGCommand()
+        ]);
+    }
+}
