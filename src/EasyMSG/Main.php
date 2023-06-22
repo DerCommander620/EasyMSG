74f8223cbff8f20fb43dcb845011983630fcb01a
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-22 07:11:16 +0200

    Update Main.php

--- src/EasyMSG/Main.php
+++ src/EasyMSG/Main.php
@@ -16,7 +16,7 @@ class Main extends PluginBase{
 
     public function onEnable(): void{
         self::getServer()->getCommandMap()->registerAll($this->getName(), $this[
-            new MSGCommand()
+            new MSGCommand
         ]);
     }
 }
