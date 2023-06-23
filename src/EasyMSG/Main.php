914ff4d21d02f27dd1111a4d7fac4eba1ee123b2
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-23 12:20:11 +0200

    Update Main.php

--- src/EasyMSG/Main.php
+++ src/EasyMSG/Main.php
@@ -1,22 +1,36 @@
 <?php
 namespace EasyMSG;
 
-use EasyMSG\Commands\MSGCommand;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
-use pocketmine\utils\SingletonTrait;
 
 class Main extends PluginBase{
-    use SingletonTrait;
 
-    public function onLoad(): void{
-        self::setInstance($this);
+    public function onEnable(): void{
         Server::getInstance()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand("tell"));
     }
 
-    public function onEnable(): void{
-        Server::getInstance()->getCommandMap()->registerAll($this->getName(), $this[
-            new MSGCommand
-        ]);
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        switch($command->getName()){
+            case "msg":
+                $player = Server::getInstance()->getPlayerExact($args[0]);
+                $message = $args([1]);
+                if(empty($args[0])){
+                    $sender->sendMessage("§cPlease enter a Player Name!");
+                }
+                if(empty($args[1])){
+                    $sender->sendMessage("§cPlease enter the Message you want to send");
+                }
+                if(!$player){
+                    $sender->sendMessage("§cPlayer not found.");
+                }else{
+                    $player->sendMessage("§bFrom " . $sender->getName() . ": §r§f" . $message);
+                    $this->getLogger()->info($sender->getName() . " send " . $player->getName() . " a message:" . $message);
+                    $sender->sendMessage("§d§lTo: §r§a" . $player->getName() . ". §r§f " . $message);
+                }
+        }
+    return true;    
     }
 }
